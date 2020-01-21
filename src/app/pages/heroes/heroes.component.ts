2babff04e5a7b28a8bb7d3f7af64c865f358d958
Eleazar <eleazarcaz@outlook.com>
2020-01-21 09:44:51 -0600

    Creamos metodo DELETE

--- src/app/pages/heroes/heroes.component.ts
+++ src/app/pages/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 import { HeroeModel } from 'src/app/models/heroe.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroes',
@@ -14,5 +15,21 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this._heroes.getHeroes().subscribe(resp => (this.Heroes = resp));
     console.log(this.Heroes);
+  }
+
+  borrarHeroe(heroe: HeroeModel, i: number) {
+    Swal.fire({
+      title: 'Borrar',
+      text: `¿Deseas borrar a ${heroe.nombre} ?`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp => {
+      if (resp.value === true) {
+        Swal.fire('Borrado', `Borrado con exito ${heroe.nombre}`, 'success');
+        this.Heroes.splice(i, 1);
+        this._heroes.borrarHeroe(heroe.id).subscribe();
+      }
+    });
   }
 }
